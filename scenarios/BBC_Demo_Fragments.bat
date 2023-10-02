da65ed517f387514c53e796b49c509465efbde8e
Andy-IL <35064377+Andy-IL@users.noreply.github.com>
2023-10-02 09:00:00 +0100

    Batch file to run on PC

---
+++ scenarios/BBC_Demo_Fragments.bat
@@ -0,0 +1,10 @@
+rem  Experimental batch file 
+rem  GHA_Demo 
+rem 
+java -Xms2g -Xmx2g -jar C:\performance_testing\apache-jmeter-5.5\bin\ApacheJMeter.jar   -n ^
+    -t C:\performance_testing\GHA_Demo\scenarios\BBC_Demo_Fragments.jmx ^
+    -l C:\performance_testing\GHA_Demo\results\result.csv ^
+    -q C:\performance_testing\GHA_Demo\properties\Short_Smoke_Test.properties ^
+    -q C:\performance_testing\GHA_Demo\properties\Smoke_Test_NFR.properties ^
+    -J includecontroller.prefix=C:\performance_testing\GHA_Demo\fragments\ ^
+    -e  -o C:\performance_testing\GHA_Demo\results\html_results
