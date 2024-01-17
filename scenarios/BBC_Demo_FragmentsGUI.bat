9edd818a2442f680ff4fa8bfb786f700a3c13fd2
Andy-IL <35064377+Andy-IL@users.noreply.github.com>
2024-01-17 16:52:47 +0000

    JM 5.6.2; gitignore tgz,zip

--- scenarios/BBC_Demo_FragmentsGUI.bat
+++ scenarios/BBC_Demo_FragmentsGUI.bat
@@ -1,12 +1,10 @@
 rem  Experimental batch file 
-rem  GHA_Demo 
+rem  GHA_Demo BBC script GUI version
 rem  uses fragments
 rem results.csv and html_results must not exist
 rem 
-java -Xms2g -Xmx2g -jar C:\performance_testing\apache-jmeter-5.5\bin\ApacheJMeter.jar   -n ^
+java -Xms2g -Xmx2g -jar C:\performance_testing\apache-jmeter-5.6.2\bin\ApacheJMeter.jar    ^
     -t C:\performance_testing\GHA_Demo\scenarios\BBC_Demo_Fragments.jmx ^
-    -l C:\performance_testing\GHA_Demo\results\result.csv ^
     -q C:\performance_testing\GHA_Demo\properties\Short_Smoke_Test.properties ^
     -q C:\performance_testing\GHA_Demo\properties\Smoke_Test_NFR.properties ^
     -J includecontroller.prefix=C:\performance_testing\GHA_Demo\fragments\ ^
-    -e  -o C:\performance_testing\GHA_Demo\results\html_results
