9a58f09481696d267adc1ed3a14895c1a257c96c
Andy-IL <35064377+Andy-IL@users.noreply.github.com>
2024-06-26 13:17:36 +0100

    apache-jmeter-5.6.2 -> apache-jmeter-5.6.3

--- scenarios/BBC_Demo_Fragments.bat
+++ scenarios/BBC_Demo_Fragments.bat
@@ -3,7 +3,7 @@ rem  GHA_Demo
 rem  uses fragments
 rem results.csv and html_results must not exist
 rem 
-java -Xms2g -Xmx2g -jar C:\performance_testing\apache-jmeter-5.5\bin\ApacheJMeter.jar   -n ^
+java -Xms2g -Xmx2g -jar C:\performance_testing\apache-jmeter-5.6.3\bin\ApacheJMeter.jar   -n ^
     -t C:\performance_testing\GHA_Demo\scenarios\BBC_Demo_Fragments.jmx ^
     -l C:\performance_testing\GHA_Demo\results\result.csv ^
     -q C:\performance_testing\GHA_Demo\properties\Short_Smoke_Test.properties ^
